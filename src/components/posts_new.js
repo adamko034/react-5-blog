f83ca4ee4dff0e8fd00f7a0d7c34df947ffe0200
Adam Kotas <akotas@future-processing.com>
2018-04-13 15:57:28 +0200

    form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,18 +1,64 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import {Field, reduxForm} from 'redux-form'
 
 class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+                {field.meta.error}
+            </div>
+        )
+    }
+    
     render() {
         return (
-            <div>
-                PostsNew!
-                <Link className="btn btn-primary" to="/">
-                    Cancel
-                </Link>
-            </div>
-
+            <form>
+                <Field 
+                    name="title"
+                    component={this.renderField}
+                    label="Title"
+                />
+                <Field 
+                    name="categories"
+                    component={this.renderField}
+                    label="Categories"
+                />
+                <Field
+                    name="content"
+                    component={this.renderField}
+                    label="Post Content"
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+function validate(values) {
+    const errors = {};
+
+    if(!values.title) {
+        errors.title = "Entar a title!"
+    }
+
+    if(!values.categories) {
+        errors.categories = "Entar some categories!"
+    }
+
+    if(!values.content) {
+        errors.content = "Entar some content!"
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);

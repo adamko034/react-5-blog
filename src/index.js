7649893e25f53a818d5b8c63dba71f517dda2684
Adam Kotas <akotas@future-processing.com>
2018-04-27 15:12:02 +0200

    delete post

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import reducers from './reducers';
 
 import PostsIndex from "./components/posts_index";
-import PostsNew from './components/posts_new'
+import PostsNew from './components/posts_new';
+import PostsShow from './components/post_show';
 
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -20,6 +21,7 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
           <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
